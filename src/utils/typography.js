37e35d897473f2062f5cdc2c22a0aff72eb21335
Isaac Pan <isaac.pan@skylinetw.com>
2019-08-27 22:50:54 +0800

    feat: add typography.js

---
+++ src/utils/typography.js
@@ -0,0 +1,5 @@
+import Typography from "typography"
+import FunstonTheme from "typography-theme-funston"
+const typography = new Typography(FunstonTheme)
+export const { scale, rhythm, options } = typography
+export default typography
