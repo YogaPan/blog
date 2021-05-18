6426190aab3e50b664aa218ae43801b21cc3c249
Galtz <galtz0321@gmail.com>
2021-05-18 13:22:23 +0800

    feat: apply customized typography

--- src/config/typography.js
+++ src/config/typography.js
@@ -2,11 +2,21 @@ import Typography from 'typography'
 import CodePlugin from 'typography-plugin-code'
 import FunstonTheme from 'typography-theme-funston'
 
-FunstonTheme.headerWeight = 500
-FunstonTheme.baseFontSize = '18px'
-FunstonTheme.plugins = [new CodePlugin()]
+const customizedTheme = {
+  ...FunstonTheme,
+  baseFontSize: '18px',
+  headerWeight: '700',
+  googleFonts: [
+    { name: 'Titillium Web', styles: ['400', '700'] },
+    { name: 'Cabin Condensed', styles: ['400', '700'] },
+    { name: 'Noto Sans TC', styles: ['400', '700'] },
+  ],
+  headerFontFamily: ['Titillium Web', 'Noto Sans TC', 'sans-serif'],
+  bodyFontFamily: ['Cabin Condensed', 'Noto Sans TC', 'georgia', 'sans-serif'],
+  plugins: [new CodePlugin()],
+}
 
-const typography = new Typography(FunstonTheme)
+const typography = new Typography(customizedTheme)
 
 export const { scale, rhythm, options } = typography
 export default typography
