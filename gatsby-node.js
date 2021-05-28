af1f20c8c1df1dda315429c4ec6cad1ddef7cb17
Galtz <galtz0321@gmail.com>
2021-05-29 05:41:38 +0800

    build: add eslint rules

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,7 +33,7 @@ exports.createPages = async ({ graphql, actions }) => {
   result.data.allMdx.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
-      component: path.resolve(`./src/templates/blog-post.js`),
+      component: path.resolve('./src/templates/blog-post.js'),
       context: {
         slug: node.fields.slug
       }
