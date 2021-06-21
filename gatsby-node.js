3151c15dc6e48e8933718f51451f07e5c1fac669
Galtz <galtz0321@gmail.com>
2021-06-21 10:12:06 +0800

    feat: table of contents

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ const { createFilePath } = require('gatsby-source-filesystem')
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === 'Mdx') {
-    const slug = createFilePath({ node, getNode, basePath: 'posts' })
+    const slug = createFilePath({ node, getNode })
     createNodeField({
       name: 'slug',
       node,
